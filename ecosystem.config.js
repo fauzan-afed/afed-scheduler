42e5cb4a69c84f4924c3010276ed5b43bf351aad
fatimahkhairul <fatimahkhairulannuar@gmail.com>
2026-03-02 03:23:13 +0000

    force 3001

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
     args: 'start',
     cwd: '/root/afed-scheduler',
     instances: 1,
+    exec_mode: 'fork',
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
@@ -12,9 +13,14 @@ module.exports = {
       NODE_ENV: 'development',
       PORT: 3001
     },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3001
+    },
     error_file: '/var/log/pm2/afed-scheduler-error.log',
     out_file: '/var/log/pm2/afed-scheduler-out.log',
     log_file: '/var/log/pm2/afed-scheduler-combined.log',
-    time: true
+    time: true,
+    merge_logs: true
   }]
 };
